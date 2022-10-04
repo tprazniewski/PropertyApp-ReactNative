07ff9f9119f6103ee17a687eed1d56cd699b957c
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-04 18:35:58 +0200

    update new screen

--- Property/screens/Gmina.js
+++ Property/screens/Gmina.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {View, Text,Button} from 'react-native'
+import {View, Text,Button, FlatList,StyleSheet} from 'react-native'
 import miasto from '../src/api/miasto'
 
 
@@ -20,12 +20,34 @@ function Second({route, navigation}) {
 
   console.log(route.params)
   return (
+
     <View>
-        <Text onPress={() => navigation.navigate('Second',{id: "hejlo"})}> This is a Third Screen Page</Text>
-        {result.map((el) => <Button title={el.City} onPress={() => navigation.navigate('Miasto',el.District)}/>
-        // {result.map((el) => <Text onPress={searchApi} style={styles.text}> We have results: {el.County}</Text>
-        )}
-    </View>  )
+    <FlatList
+      keyExtractor={(item, index) => index.toString()}
+      contentContainerStyle={styles.contactList}
+      data={result}
+      // data={user}
+      renderItem={( {item} ) => (
+        <Button
+          title={item.City}
+          onPress={() => navigation.navigate("Miasto", item.City)}
+        />
+      )}
+    />
+  </View>
+
+    // <View>
+    //     <Text onPress={() => navigation.navigate('Second',{id: "hejlo"})}> This is a Third Screen Page</Text>
+    //     {result.map((el) => <Button title={el.City} onPress={() => navigation.navigate('Miasto',el.District)}/>
+    //     // {result.map((el) => <Text onPress={searchApi} style={styles.text}> We have results: {el.County}</Text>
+    //     )}
+    // </View> 
+     )
 }
 
 export default Second
+const styles = StyleSheet.create({
+  text: {
+      color: `#7fff00`
+  },
+});
--- Property/screens/Powiaty.js
+++ Property/screens/Powiaty.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {View, Text,Button} from 'react-native'
+import {View, Text,Button, FlatList,StyleSheet} from 'react-native'
 import gminy from '../src/api/gmina'
 
 function Second({route, navigation}) {
@@ -20,12 +20,34 @@ function Second({route, navigation}) {
 
   console.log(route.params)
   return (
+
     <View>
-        <Text onPress={() => navigation.navigate('Second',{id: "hejlo"})}> This is a Third Screen Page</Text>
-        {result.map((el) => <Button title={el.Municipality} onPress={() => navigation.navigate('Gmina',el.Municipality)}/>
-        // {result.map((el) => <Text onPress={searchApi} style={styles.text}> We have results: {el.County}</Text>
-        )}
-    </View>  )
+    <FlatList
+      keyExtractor={(item, index) => index.toString()}
+      contentContainerStyle={styles.contactList}
+      data={result}
+      // data={user}
+      renderItem={( {item} ) => (
+        <Button
+          title={item.Municipality}
+          onPress={() => navigation.navigate("Gmina", item.Municipality)}
+        />
+      )}
+    />
+  </View>
+
+    // <View>
+    //     <Text onPress={() => navigation.navigate('Second',{id: "hejlo"})}> This is a Third Screen Page</Text>
+    //     {result.map((el) => <Button title={el.Municipality} onPress={() => navigation.navigate('Gmina',el.Municipality)}/>
+    //     // {result.map((el) => <Text onPress={searchApi} style={styles.text}> We have results: {el.County}</Text>
+    //     )}
+    // </View> 
+     )
 }
 
 export default Second
+const styles = StyleSheet.create({
+  text: {
+      color: `#7fff00`
+  },
+});
